99ac248bb1b2a8700c01493fa422cca84eccdf3b
efraimrodrigues <efraimnaassom@gmail.com>
2015-04-27 12:32:47 -0300

    Update kill.c
    Comando implementado.

--- UFERSAOS/src/kill.c
+++ UFERSAOS/src/kill.c
@@ -5,6 +5,12 @@
  *      Author: hugo
  */
 
-void killthis() {
-	printf("killing...\n");
+void killthis(int argc, char * argv[]) {
+	if(argc == 2)
+		if(!kill(atoi(argv[1]),SIGQUIT))
+			printf("Processo %s finalizado.\n",argv[1]);
+		else
+			printf("Processo inexiste ou impossibilitado de ser finalizado.\n");
+	else
+		printf("Verifique a quantidade de argumentos passados.");
 }
