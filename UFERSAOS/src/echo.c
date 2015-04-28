5a2664b2bedfaf5304d5a3c5812025272e476bfb
efraimrodrigues <efraimnaassom@gmail.com>
2015-04-28 00:42:55 -0300

    Update echo.c
    Bug corrigido na linha 11.

--- UFERSAOS/src/echo.c
+++ UFERSAOS/src/echo.c
@@ -6,13 +6,11 @@
  */
 
 void echo(int argc, char * argv []) {
-	for(int i=1; i<argc; i++)
+	for(int i=1; i<=argc; i++)
 	{
-		write(1, argv[i], sizeof(argv[i]) - 1);
+		write(1, argv[i], strlen(argv[i]));
 		write(1, " ", 1);
 	}
 
 	write(1, "\n", 1);
-
-	return 0;
 }
