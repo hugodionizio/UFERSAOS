82ad852fe203feaaaac7c63519856dcb4a2dbfa6
hugodionizio <hugodionizio@users.noreply.github.com>
2015-04-28 18:17:31 -0300

    Create UFERSAOS.h

---
+++ UFERSAOS/src/UFERSAOS.h
@@ -0,0 +1,22 @@
+/*
+ * UFERSAOS.h
+ *
+ *  Created on: 28/04/2015
+ *      Author: hugo
+ */
+
+#ifndef UFERSAOS_H_
+#define UFERSAOS_H_
+
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "Auxiliar.h"
+#include "commands.h"
+
+#endif /* UFERSAOS_H_ */
