5b022d2feaf60a61abecaa6a64af2351e8891d61
efraimrodrigues <efraimnaassom@gmail.com>
2015-04-28 00:40:40 -0300

    Update main.c
    Nova maneira de dividir as entradas.

--- UFERSAOS/src/main.c
+++ UFERSAOS/src/main.c
@@ -1,7 +1,7 @@
 /*
  * Projeto: UFERSA OS
  * Algoritmo: main.c
- * Função: UFERSA OS's Shell
+ * FunÃ§Ã£o: UFERSA OS's Shell
  *  Created on: 21/04/2015
  *      Authors: Efraim Rodrigues
  *      		 Hugo Dionizio Santos
@@ -25,7 +25,6 @@ int main(int argc, char **argv) {
 	int argint = MAXCOMMANDS, i;
 
 	char buffer[MAXBUFFER];
-	char command[20] = "";
 	char commands[][MAXCOMMANDS] = { "ls", "cd", "echo", "ps", "kill",
 						  "exit", "alias", "unalias", "export", "unset"};
 
@@ -47,12 +46,16 @@ int main(int argc, char **argv) {
 			fgets(buffer, MAXBUFFER, stdin);
 			buffer[strlen(buffer)-1] = '\0';
 		}
-		printf("Número de comandos: %d\n", contaEspacos(buffer)+1);
-		strcpy(command, listarComandos(buffer, 0));
+		int numeroDeEntradas = contaEspacos(buffer)+1;
 
+		printf("Número de entradas: %d\n", numeroDeEntradas);
+
+		char ** string = divideString(buffer);
+
+		//Direciona o fluxo de execução para o comando escolhido.
 		for(i = 0; i < MAXCOMMANDS; i++) {
 			if (argc >= 1) {
-				if (strcmp(commands[i], command) == 0) {
+				if (strcmp(commands[i], string[0]) == 0) {
 					argint = i;
 					break;
 				}
@@ -61,23 +64,22 @@ int main(int argc, char **argv) {
 		if (i == MAXCOMMANDS) {
 			argint = MAXCOMMANDS;
 		}
-		printf("Comando escolhido: %s (%d)\n", command, argint);
+		printf("Comando escolhido: %s (%d)\n", string[0], argint);
 
 		switch (argint) {
 			case 0: ls(); break;
-			case 1: cd(); break;
-			case 2: echo(); break;
+			case 1: cd(numeroDeEntradas-1,string); break;
+			case 2: echo(numeroDeEntradas-1,string); break;
 			case 3: ps(); break;
-			case 4: killthis(); break;
+			case 4: killthis(numeroDeEntradas-1,string); break;
 			case 5: exitShell(); break;
 			case 6: alias(argv[2]); break;
 			case 7: unalias(argv[2]); break;
 			case 8: export(argv[2]); break;
 			case 9: unset(argv[2]); break;
-			default: printf("Hem?! Comando desconhecido...\n");
+			default: system(buffer);
 		}
 	} while (argc <= 1 && argint != 5);
 
 	return EXIT_SUCCESS;
 }
-
