c06962f6730d00c3ce8577ff2e4c2d2b51d5954d
hugodionizio <hugodionizio@users.noreply.github.com>
2015-04-28 01:11:56 -0300

    Update export.c

--- UFERSAOS/src/export.c
+++ UFERSAOS/src/export.c
@@ -6,7 +6,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int export (char *argv) {
+extern char **environ;
+
+int export(char *argv[])
+{
+	//extern char aliasVar[80];
+//	struct aliasent *alias;
+
+  int count = 0;
+
+  printf("exporting...\n");
+  printf("\n");
+  while(environ[count] != NULL)
+  {
+    printf("[%s] :: ", environ[count]);
+    count++;
+  }
+
+  char *val = getenv("USER");
+  printf("\n\nCurrent value of environment variable USER is [%s]\n",val);
+
+  if(setenv("USER","Arora",1))
+  {
+    printf("\n setenv() failed\n");
+    return 1;
+  }
+
+  printf("\n Successfully Added a new value to existing environment variable USER\n");
+
+  val = getenv("USER");
+  printf("\nNew value of environment variable USER is [%s]\n",val);
+
+  /*while(1)
+  {
+    sleep(2);
+  }*/
+
+  return 0;
+}
+
+int export_old (char *argv) {
 	extern char var[80];
 
 	printf("exporting...\n");
